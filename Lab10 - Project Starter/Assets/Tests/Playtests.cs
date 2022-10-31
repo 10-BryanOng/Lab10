7f7ef8d140845ada6bc009c9afd979cf669ee7b1
10-BryanOng <bryanong0102@yahoo.com.sg>
2022-10-31 16:39:39 +0800

    Created code for square root and power
    Created the code for square root and power and created tests for subtraction, Multiply, Division, Power and Square Root

--- Lab10 - Project Starter/Assets/Tests/Playtests.cs
+++ Lab10 - Project Starter/Assets/Tests/Playtests.cs
@@ -10,13 +10,13 @@ namespace Tests
     {
         private float result = 0.0f;
 
+        //Addition
         [Test]
         public void TestAddition()
         {
             result = Calculator.CalculatePair(5, 2, "+");
             Assert.AreEqual(result, 7);
         }
-
         [UnityTest]
         public IEnumerator TestUnityAddition()
         {
@@ -24,6 +24,85 @@ namespace Tests
 
             result = Calculator.CalculatePair(5, 2, "+");
             Assert.AreEqual(result, 7);
+        }
+
+        //Subtraction
+        [Test]
+        public void TestSubtraction()
+        {
+            result = Calculator.CalculatePair(5, 2, "-");
+            Assert.AreEqual(result, 3);
+        }
+        [UnityTest]
+        public IEnumerator TestUnitySubtraction()
+        {
+            yield return null;
+
+            result = Calculator.CalculatePair(5, 2, "-");
+            Assert.AreEqual(result, 3);
+        }
+
+        //Multiplication
+        [Test]
+        public void TestMultiply()
+        {
+            result = Calculator.CalculatePair(5, 2, "*");
+            Assert.AreEqual(result, 10);
+        }
+        [UnityTest]
+        public IEnumerator TestUnityMultiply()
+        {
+            yield return null;
+
+            result = Calculator.CalculatePair(5, 2, "*");
+            Assert.AreEqual(result, 10);
+        }
+
+        //Division
+        [Test]
+        public void TestDivision()
+        {
+            result = Calculator.CalculatePair(10, 2, "/");
+            Assert.AreEqual(result, 5);
+        }
+        [UnityTest]
+        public IEnumerator TestUnityDivision()
+        {
+            yield return null;
+
+            result = Calculator.CalculatePair(10, 2, "/");
+            Assert.AreEqual(result, 5);
+        }
+
+        [Test]
+        public void TestPower()
+        {
+            result = Calculator.CalculatePair(5, 2, "^");
+            Assert.AreEqual(result, 25);
+        }
+
+        [UnityTest]
+        public IEnumerator TestUnityPower()
+        {
+            yield return null;
+
+            result = Calculator.CalculatePair(5, 2, "^");
+            Assert.AreEqual(result, 25);
+        }
+
+        //Square Root
+        public void TestSQRT()
+        {
+            result = Calculator.CalculatePair(0, 100, "√");
+            Assert.AreEqual(result, 10);
+        }
+        [UnityTest]
+        public IEnumerator TestUnitySQRT()
+        {
+            yield return null;
+
+            result = Calculator.CalculatePair(0, 100, "√");
+            Assert.AreEqual(result, 10);
         }
     }
 }
